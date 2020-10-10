6f8a0646c1ffa98dd416fe91e17d90d585a6b09e
kmikhails <kmikhails@yandex.ru>
2020-10-10 21:52:16 +0500

    added NewService

---
+++ src/main/java/com/kmikhails/paymentaccount/service/NewService.java
@@ -0,0 +1,12 @@
+package com.kmikhails.paymentaccount.service;
+
+public class NewService {
+
+    public void printValue(String value) {
+        System.out.println(value);
+    }
+
+    public Integer getIntFromValue(String value) {
+        return Integer.parseInt(value);
+    }
+}
